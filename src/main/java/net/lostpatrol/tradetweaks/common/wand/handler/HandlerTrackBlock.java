eecb55e09e358e487591baaab4d96f8b25b027c7
LostPatrol <2823931730@qq.com>
2026-04-30 20:43:30 +0800

    Add potential jobsite tracking

--- src/main/java/net/lostpatrol/tradetweaks/common/wand/handler/HandlerTrackBlock.java
+++ src/main/java/net/lostpatrol/tradetweaks/common/wand/handler/HandlerTrackBlock.java
@@ -23,6 +23,9 @@ public class HandlerTrackBlock {
     public static final float R = 1.0F;
     public static final float G = 0.0F;
     public static final float B = 0.0F;
+    public static final float POTENTIAL_R = 0.0F;
+    public static final float POTENTIAL_G = 1.0F;
+    public static final float POTENTIAL_B = 0.0F;
 
     public static Component TRACK_BLOCK_SUCCESS(BlockPos pos) {
         return Component.translatable("tradetweaks.emerald_wand.success.track_block", pos.getX(), pos.getY(), pos.getZ())
@@ -30,29 +33,42 @@ public class HandlerTrackBlock {
                         .withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Component.translatable("tradetweaks.emerald_wand.text.copy"))));
     }
 
-    public static final Component TRACK_BLOCK_FAIL = Component.translatable("tradetweaks.emerald_wand.fail.villager_not_occupied").withStyle(ChatFormatting.RED);
-    public static final Component TRACK_BLOCK_FAIL_ERR = Component.translatable("tradetweaks.emerald_wand.fail.block_not_exist").withStyle(ChatFormatting.RED);
+    public static final Component TRACK_BLOCK_FAIL = Component.translatable("tradetweaks.emerald_wand.fail.no_potential_jobsite").withStyle(ChatFormatting.RED);
 
     public static InteractionResult handle(Player player, Villager villager) {
-        if (VillagerUtil.isUnemployed(villager) || VillagerUtil.isNitwit(villager)) {
+        if (VillagerUtil.isNitwit(villager)) {
             player.displayClientMessage(TRACK_BLOCK_FAIL, true);
             return InteractionResult.SUCCESS;
         }
+
+        if (VillagerUtil.isUnemployed(villager)) {
+            Optional<GlobalPos> potentialJobSite = villager.getBrain().getMemory(MemoryModuleType.POTENTIAL_JOB_SITE);
+            if (potentialJobSite.isPresent()) {
+                highlightBlock(player, potentialJobSite.get().pos(), POTENTIAL_R, POTENTIAL_G, POTENTIAL_B);
+            } else {
+                player.displayClientMessage(TRACK_BLOCK_FAIL, true);
+            }
+            return InteractionResult.SUCCESS;
+        }
+
         Optional<GlobalPos> jobSite = villager.getBrain().getMemory(MemoryModuleType.JOB_SITE);
         if (jobSite.isPresent()) {
-            BlockPos pos = jobSite.get().pos();
-            player.sendSystemMessage(TRACK_BLOCK_SUCCESS(pos));
-            if (player instanceof ServerPlayer serverPlayer) {
-                NetworkHandler.sendBlockHighlightToPlayer(
-                        serverPlayer,
-                        new PacketBlockHighlight(pos, DURATION_TICKS, R, G, B)
-                );
-            }
+            highlightBlock(player, jobSite.get().pos(), R, G, B);
         }
         else {
-            player.displayClientMessage(TRACK_BLOCK_FAIL_ERR, true);
+            player.displayClientMessage(TRACK_BLOCK_FAIL, true);
         }
         return InteractionResult.SUCCESS;
+    }
+
+    private static void highlightBlock(Player player, BlockPos pos, float red, float green, float blue) {
+        player.sendSystemMessage(TRACK_BLOCK_SUCCESS(pos));
+        if (player instanceof ServerPlayer serverPlayer) {
+            NetworkHandler.sendBlockHighlightToPlayer(
+                    serverPlayer,
+                    new PacketBlockHighlight(pos, DURATION_TICKS, red, green, blue)
+            );
+        }
     }
 }
 
