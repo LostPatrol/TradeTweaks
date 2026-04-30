e0e1439118b4aa96ef749c50d52cad845c991c73
LostPatrol <2823931730@qq.com>
2026-04-30 19:58:10 +0800

    some sync

--- src/main/java/net/lostpatrol/tradetweaks/client/events/WandScrollHandler.java
+++ src/main/java/net/lostpatrol/tradetweaks/client/events/WandScrollHandler.java
@@ -2,6 +2,7 @@ package net.lostpatrol.tradetweaks.client.events;
 
 import net.lostpatrol.tradetweaks.common.wand.EmeraldWand;
 import net.minecraft.client.Minecraft;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.neoforged.neoforge.client.event.InputEvent;
@@ -16,16 +17,16 @@ public class WandScrollHandler {
         if (player == null || !player.isShiftKeyDown()) return;
         if (scroll == (double)0.0F) return;
 
-        net.minecraft.world.InteractionHand hand = null;
+        InteractionHand hand = null;
         ItemStack heldItem = player.getMainHandItem();
 
         if (heldItem.getItem() instanceof EmeraldWand) {
-            hand = net.minecraft.world.InteractionHand.MAIN_HAND;
+            hand = InteractionHand.MAIN_HAND;
         } else {
             ItemStack offhandItem = player.getOffhandItem();
             if (offhandItem.getItem() instanceof EmeraldWand) {
                 heldItem = offhandItem;
-                hand = net.minecraft.world.InteractionHand.OFF_HAND;
+                hand = InteractionHand.OFF_HAND;
             }
         }
 
--- src/main/java/net/lostpatrol/tradetweaks/network/handler/HandlerWandModeSwitch.java
+++ src/main/java/net/lostpatrol/tradetweaks/network/handler/HandlerWandModeSwitch.java
@@ -2,6 +2,7 @@ package net.lostpatrol.tradetweaks.network.handler;
 
 import net.lostpatrol.tradetweaks.common.wand.EmeraldWand;
 import net.lostpatrol.tradetweaks.network.packet.PacketWandModeSwitch;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.neoforged.neoforge.network.handling.IPayloadContext;
@@ -13,13 +14,13 @@ public class HandlerWandModeSwitch {
         ctx.enqueueWork(() -> {
             Player player = ctx.player();
             ItemStack stack = player.getItemInHand(packet.getHand());
-            if (stack.getItem() instanceof EmeraldWand wand) {
+            if (stack.getItem() instanceof EmeraldWand) {
                 handleModeSwitchServer(stack, player, packet.isForward(), packet.getHand());
             }
         });
     }
 
-    public static void handleModeSwitchServer(ItemStack stack, Player player, boolean forward, net.minecraft.world.InteractionHand hand) {
+    public static void handleModeSwitchServer(ItemStack stack, Player player, boolean forward, InteractionHand hand) {
         EmeraldWand.WandMode current = getMode(stack);
         EmeraldWand.WandMode newMode = forward ? current.next() : current.previous();
         setMode(stack, newMode);
